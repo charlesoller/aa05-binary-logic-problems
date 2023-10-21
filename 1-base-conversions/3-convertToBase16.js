c4c71fef6487aa84204fe5b6c90e0902528cc0e0
charlesoller <charlesrello@gmail.com>
2023-10-21 13:51:27 -0400

    3 done

--- 1-base-conversions/3-convertToBase16.js
+++ 1-base-conversions/3-convertToBase16.js
@@ -3,7 +3,12 @@
 /******************************************************************************/
 
 const convertToBase16 = element => {
-  // Your code here 
+  if(typeof element === 'number'){
+    return `0x${element.toString(16)}`;
+  } else {
+    element = parseInt(element.slice(2), 2)
+    return `0x${element.toString(16)}`;
+  }
 };
 
 /******************************************************************************/
